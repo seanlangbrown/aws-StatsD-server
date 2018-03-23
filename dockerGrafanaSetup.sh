521535cbb803928e2cc8841ef59753a288bd83e6
Seanlangbrown <seanlangbrown@gmail.com>
2018-03-23 13:48:08 -0700

    sudo docker commands

--- dockerGrafanaSetup.sh
+++ dockerGrafanaSetup.sh
@@ -15,10 +15,10 @@ npm install -g artillery
 #update and install docker
 sudo yum install -y docker
 sudo service docker start
-sudo usermod -a -G docker ec2-user
+sudo usermod -a -G docker ec2-user #cahnges will be applied on next login
 
 #create docker network
-docker network create statsdnetwork
+sudo docker network create statsdnetwork
 
 #allow network forwarding
 sudo sysctl net.ipv4.conf.all.forwarding=1
@@ -34,9 +34,9 @@ mkdir -p ~/grafana/var/lib/grafana
 
 #start statsd, graphite and grafana
 #see documentation: https://github.com/hopsoft/docker-graphite-statsd
-docker run -d --name graphite --restart=always --network statsdnetwork -p 80:80 -p 2003-2004:2003-2004 -p 2023-2024:2023-2024 -p 8125:8125/udp -p 8126:8126 -v ~/graphite/opt/graphite/conf:/opt/graphite/conf -v ~/graphite/opt/graphite/storage:/opt/graphite/storage -v ~/statsd/opt/statsd:/opt/statsd graphiteapp/graphite-statsd
-docker run -d -p 3000:3000 --network statsdnetwork --name grafana -e"GF_SECURITY_ADMIN_PASSWORD=${password}" -v ~/grafana/var/lib/grafana:/var/lib/grafana grafana/grafana
+sudo docker run -d --name graphite --restart=always --network statsdnetwork -p 80:80 -p 2003-2004:2003-2004 -p 2023-2024:2023-2024 -p 8125:8125/udp -p 8126:8126 -v ~/graphite/opt/graphite/conf:/opt/graphite/conf -v ~/graphite/opt/graphite/storage:/opt/graphite/storage -v ~/statsd/opt/statsd:/opt/statsd graphiteapp/graphite-statsd
+sudo docker run -d -p 3000:3000 --network statsdnetwork --name grafana -e"GF_SECURITY_ADMIN_PASSWORD=${password}" -v ~/grafana/var/lib/grafana:/var/lib/grafana grafana/grafana
 
 #verify setup
-docker ps -a
+sudo docker ps -a
 echo "grafana login: admin, ${password}.  set data source to http://graphite"
